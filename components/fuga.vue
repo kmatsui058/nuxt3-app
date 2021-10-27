660fcdc4f07fc04c3cf9d443a83b2ddd207ac8eb
kmatsui058 <kmatsui058@gmail.com>
2021-10-27 23:45:52 +0900

    first commit

---
+++ components/fuga.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>{{ fuga }}</div>
+</template>
+<script lang="ts" setup>
+const props = defineProps({fuga: {type: String, required: true}})
+const fuga = computed(()=>props.fuga)
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{value}}{{data}}
+        <fuga :fuga="0"/>
+    </div>
+</template>
+<script setup lang="ts">
+import fuga from '@/components/fuga.vue'
+const tu = {ho: 'ho'}
+const te = tu.fu
+const value = ref('hoge')
+const { data } = await useAsyncData('count', async ()=>{ return 1 })
+</script>
+
